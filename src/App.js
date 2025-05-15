381c181f60734b5783374c4816fed5211f0d3092
joaodsantiagom <joaodsantiagom@gmail.com>
2025-05-15 15:11:41 +0100

    changed details

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import './styles/App.scss';
+import './styles/app.scss';
 import './styles/_variables.scss';
 import './styles/_mixins.scss';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
